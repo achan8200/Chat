4d207a4b05662f581c5d3d5ba4670bdcc32d1c4d
Allan Chan <allanchan@cpp.edu>
2023-03-07 11:16:53 -0800

    Update

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -23,7 +23,7 @@ const TeamChannelPreview = ({ channel, type, setIsCreating, setIsEditing, setTog
                     name={members[0]?.user?.fullName || members[0]?.user?.id}
                     size={24}
                 />
-                <p>{members[0]?.user?.fullName}</p>
+                <p>{members[0]?.user?.fullName || members[0]?.user?.id}</p>
             </div>
         )
     }
--- client/src/components/TeamMessage.jsx
+++ client/src/components/TeamMessage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { MessageTeam, useMessageContext } from 'stream-chat-react';
 
 const TeamMessage = () => {
+    const { handleOpenThread, message } = useMessageContext();
+
     return (
-        <div>
-            TeamMessage
-        </div>
+        <MessageTeam
+            message={{ ...message, user: {}}}
+            // handleOpenThread={}
+        />
     )
 }
 
